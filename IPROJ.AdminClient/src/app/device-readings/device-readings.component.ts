cde645544c6d529bf54d6af5705f02fadf8ac1dd
margadz <mgadzalski@gmail.com>
2018-04-11 18:00:36 +0200

    zakres dat

--- IPROJ.AdminClient/src/app/device-readings/device-readings.component.ts
+++ IPROJ.AdminClient/src/app/device-readings/device-readings.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit, Input, Inject} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import { DeviceReading} from '../deviceReading';
 import { DeviceReadingService } from '../device-reading.service';
 import {ActivatedRoute} from '@angular/router';
 import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 import {Observable} from 'rxjs/Observable';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
 
 const equals = (one: NgbDateStruct, two: NgbDateStruct) =>
   one && two && two.year === one.year && two.month === one.month && two.day === one.day;
@@ -16,26 +17,18 @@ const after = (one: NgbDateStruct, two: NgbDateStruct) =>
   !one || !two ? false : one.year === two.year ? one.month === two.month ? one.day === two.day
     ? false : one.day > two.day : one.month > two.month : one.year > two.year;
 
-const equalsDate = (one: NgbDateStruct, two: Date) => one && two && two.getFullYear() === one.year && two.getMonth() === one.month && two.getDay() === one.day;
-
-const beforeDate = (one: NgbDateStruct, two: Date) =>
-  !one || !two ? false : one.year === two.getFullYear() ? one.month === two.getMonth() ? one.day === two.getDay()
-    ? false : one.day < two.getDay() : one.month < two.getMonth() : one.year < two.getFullYear();
-
-const afterDate = (one: NgbDateStruct, two: Date) =>
-  !one || !two ? false : one.year === two.getFullYear() ? one.month === two.getMonth() ? one.day === two.getDay()
-    ? false : one.day > two.getDay() : one.month > two.getMonth() : one.year > two.getFullYear();
-
 @Component({
   selector: 'app-device-readings',
   templateUrl: './device-readings.component.html',
   styleUrls: ['./device-readings.component.css']
 })
 export class DeviceReadingsComponent implements OnInit {
+  private _total$: Observable<number>;
   readings:  DeviceReading[];
   hoveredDate: NgbDateStruct;
   fromDate: NgbDateStruct;
   toDate: NgbDateStruct;
+  subject: ReplaySubject<number>;
 
   constructor(
     private deviceReadingService: DeviceReadingService,
@@ -43,10 +36,18 @@ export class DeviceReadingsComponent implements OnInit {
     calendar: NgbCalendar) {
     this.fromDate = calendar.getToday();
     this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    this.subject = new ReplaySubject<number>();
   }
 
   ngOnInit() {
     this.getReadings();
+  }
+
+  get total$(): Observable<number> {
+    if (!this._total$) {
+      this._total$ = this.subject.asObservable();
+    }
+    return this._total$;
   }
 
   getReadings() {
@@ -54,17 +55,19 @@ export class DeviceReadingsComponent implements OnInit {
     this.deviceReadingService.getReadingFor(id).then(result => this.readings = result);
   }
 
-  getDate(reading: DeviceReading): string{
-    return reading.readingTimeStamp.getDate().toString();
-  }
 
-  getTotal(): Observable<number>{
+  getTotal(): number {
     let result = 0;
-    for (const reading of this.readings){
+    const fromDate = this.getDate(this.fromDate);
+    fromDate.setHours(0,0,0,0)
+    const toDate = this.getDate(this.toDate);
+    toDate.setHours(0,0,0,0)
+    for (const reading of this.readings) {
       const date = new Date(reading.readingTimeStamp);
-      /*if ((equalsDate(this.fromDate, date) || afterDate(this.fromDate, date)) &&
-        (equalsDate(this.fromDate, date) || beforeDate(this.fromDate, date))) {*/
-         result += reading.value;
+      date.setHours(0,0,0,0)
+      if (date >= fromDate && date <= toDate) {
+        result += reading.value;
+      }
     }
     return result;
   }
@@ -78,6 +81,13 @@ export class DeviceReadingsComponent implements OnInit {
       this.toDate = null;
       this.fromDate = date;
     }
+    if (this.fromDate && this.toDate){
+      this.subject.next(this.getTotal());
+    }
+  }
+
+  getDate(date: NgbDateStruct): Date{
+    return new Date(date.year + '-' + date.month + '-' + date.day);
   }
 
   isHovered = date => this.fromDate && !this.toDate && this.hoveredDate && after(date, this.fromDate) && before(date, this.hoveredDate);
